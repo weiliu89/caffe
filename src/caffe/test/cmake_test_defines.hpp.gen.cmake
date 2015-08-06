bde13b5d7cc1b52af91195e214ee0b94e8984bc6
Dan <dtmoodie@gmail.com>
2015-08-06 14:05:08 -0400

    Wndows fixes.

---
+++ src/caffe/test/cmake_test_defines.hpp.gen.cmake
@@ -0,0 +1,4 @@
+#define CUDA_TEST_DEVICE -1
+#define CMAKE_SOURCE_DIR "D:/libs/caffe/src/"
+#define EXAMPLES_SOURCE_DIR "D:/libs/caffe/examples/"
+#define CMAKE_EXT ".gen.cmake"
---
+++ src/caffe/test/test_data/sample_data_list.txt.gen.cmake
@@ -0,0 +1,2 @@
+D:/libs/caffe/src/caffe/test/test_data/sample_data.h5
+D:/libs/caffe/src/caffe/test/test_data/sample_data_2_gzip.h5
