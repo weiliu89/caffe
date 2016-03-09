b6f945d101fccf3d9f1dc4495161276324af0458
Dan Moodie <dtmoodie@gmail.com>
2016-03-09 11:44:44 -0500

    Windows fixes.

---
+++ src/caffe/layers/python_layer.cpp
@@ -0,0 +1,41 @@
+#include "caffe/layers/python_layer.hpp"
+using namespace caffe;
+
+
+template<typename Dtype> PythonLayer<Dtype>::PythonLayer(PyObject* self, const LayerParameter& param)
+    : Layer<Dtype>(param), self_(bp::handle<>(bp::borrowed(self))) 
+{ 
+
+}
+
+template<typename Dtype> void PythonLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+    // Disallow PythonLayer in MultiGPU training stage, due to GIL issues
+    // Details: https://github.com/BVLC/caffe/issues/2936
+    if (this->phase_ == TRAIN && Caffe::solver_count() > 1
+        && !ShareInParallel()) {
+        LOG(FATAL) << "PythonLayer is not implemented in Multi-GPU training";
+    }
+    self_.attr("param_str") = bp::str(
+        this->layer_param_.python_param().param_str());
+    self_.attr("setup")(bottom, top);
+}
+
+
+template<typename Dtype> void PythonLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+    self_.attr("reshape")(bottom, top);
+}
+
+template<typename Dtype> void PythonLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+    self_.attr("forward")(bottom, top);
+}
+
+template<typename Dtype> void PythonLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    self_.attr("backward")(top, propagate_down, bottom);
+}
+
+template class PythonLayer<float>;
+template class PythonLayer<double>;
---
+++ src/caffe/proto/caffe.pb.cpp
@@ -0,0 +1,30 @@
+#include "caffe/proto/caffe_pb.h"
+
+using namespace caffe;
+
+LayerParameter* LayerParameter::default_instance_;
+TransformationParameter* TransformationParameter::default_instance_;
+AccuracyParameter* AccuracyParameter::default_instance_;
+ArgMaxParameter* ArgMaxParameter::default_instance_;
+ConcatParameter* ConcatParameter::default_instance_;
+BatchNormParameter* BatchNormParameter::default_instance_;
+ContrastiveLossParameter* ContrastiveLossParameter::default_instance_;
+ConvolutionParameter* ConvolutionParameter::default_instance_;
+DataParameter* DataParameter::default_instance_;
+DropoutParameter* DropoutParameter::default_instance_;
+DummyDataParameter* DummyDataParameter::default_instance_;
+EltwiseParameter* EltwiseParameter::default_instance_;
+EmbedParameter* EmbedParameter::default_instance_;
+ExpParameter* ExpParameter::default_instance_;
+FlattenParameter* FlattenParameter::default_instance_;
+HDF5DataParameter* HDF5DataParameter::default_instance_;
+HDF5OutputParameter* HDF5OutputParameter::default_instance_;
+HingeLossParameter* HingeLossParameter::default_instance_;
+ImageDataParameter* ImageDataParameter::default_instance_;
+InfogainLossParameter* InfogainLossParameter::default_instance_;
+InnerProductParameter* InnerProductParameter::default_instance_;
+LogParameter* LogParameter::default_instance_;
+LRNParameter* LRNParameter::default_instance_;
+MemoryDataParameter* MemoryDataParameter::default_instance_;
+MVNParameter* MVNParameter::default_instance_;
+PoolingParameter* PoolingParameter::default_instance_;
