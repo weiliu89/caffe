21c70735c7debfbbdca87be31f7810d24ac7be80
dan <dtmoodie@gmail.com>
2016-08-04 10:15:25 -0500

    Python 3 support. _caffe.py required to load caffe dll on windows correctly.

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -4,5 +4,7 @@ from ._caffe import __version__
 from .proto.caffe_pb2 import TRAIN, TEST
 from .classifier import Classifier
 from .detector import Detector
-from . import io
+import sys
+if sys.version_info[0] < 3:
+	from . import io
 from .net_spec import layers, params, NetSpec, to_proto
