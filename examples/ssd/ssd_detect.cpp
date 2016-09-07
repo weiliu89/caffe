c2ce08224d1767b1a364e8ae9c4c6824d8665e03
sungjun.choi <sungjun.choi@navercorp.com>
2016-09-07 16:43:21 +0900

    modify ssd_detect travis error

--- examples/ssd/ssd_detect.cpp
+++ examples/ssd/ssd_detect.cpp
@@ -269,8 +269,7 @@ void Detector::Preprocess(const cv::Mat& img,
   /* Convert the input image to the input image format of the network. */
   if (img.channels() == num_channels_) {
     sample.upload(img);
-  }
-  else {
+  } else {
     g_img.upload(img);
     if (g_img.channels() == 3 && num_channels_ == 1)
       cv::gpu::cvtColor(g_img, sample, cv::COLOR_BGR2GRAY);
@@ -438,7 +437,9 @@ int main(int argc, char** argv) {
   operating_time += static_cast<double>(end_point.tv_usec) / 1000000.0;
   operating_time -= static_cast<double>(start_point.tv_sec);
   operating_time -= static_cast<double>(start_point.tv_usec) / 1000000.0;
-  printf("%d Frame, %f, %f fps\n", iFrame, operating_time, static_cast<float>(iFrame) / operating_time);
+  printf("%d Frame", iFrame);
+  printf(", %f", operating_time);
+  printf(", %f fps\n", static_cast<float>(iFrame) / operating_time);
   return 0;
 }
 #else
