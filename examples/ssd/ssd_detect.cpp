59ef7a12cb720f43e4448511a8b85652afc14e52
sungjun.choi <sungjun.choi@navercorp.com>
2016-09-07 11:15:34 +0900

    add gpumat mode for detect

--- examples/ssd/ssd_detect.cpp
+++ examples/ssd/ssd_detect.cpp
@@ -12,9 +12,13 @@
 //    folder/video1.mp4
 //    folder/video2.mp4
 //
+
+#define USE_OPENCV_GPU 1
+
 #include <caffe/caffe.hpp>
 #ifdef USE_OPENCV
 #include <opencv2/core/core.hpp>
+#include <opencv2/gpu/gpu.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #endif  // USE_OPENCV
@@ -28,6 +32,7 @@
 
 #ifdef USE_OPENCV
 using namespace caffe;  // NOLINT(build/namespaces)
+using namespace cv;
 
 class Detector {
  public:
@@ -36,21 +41,33 @@ class Detector {
            const string& mean_file,
            const string& mean_value);
 
-  std::vector<vector<float> > Detect(const cv::Mat& img);
+  std::vector<vector<float> > Detect(const Mat& img);
 
  private:
   void SetMean(const string& mean_file, const string& mean_value);
 
-  void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+  void WrapInputLayer(std::vector<Mat>* input_channels);
+  void WrapInputLayer(std::vector<gpu::GpuMat>* input_channels);
 
-  void Preprocess(const cv::Mat& img,
-                  std::vector<cv::Mat>* input_channels);
+  void Preprocess(const Mat& img,
+                  std::vector<Mat>* input_channels);
+  void Preprocess(const Mat& img,
+                    std::vector<gpu::GpuMat>* input_channels);
 
  private:
   shared_ptr<Net<float> > net_;
-  cv::Size input_geometry_;
+  Size input_geometry_;
   int num_channels_;
-  cv::Mat mean_;
+  Mat mean_;
+  
+  std::vector<gpu::GpuMat> input_channels_gpu;
+  
+  gpu::GpuMat mean_gpu;
+  gpu::GpuMat g_img;
+  gpu::GpuMat sample;
+  gpu::GpuMat sample_resized;  
+  gpu::GpuMat sample_float;
+  gpu::GpuMat sample_normalized;
 };
 
 Detector::Detector(const string& model_file,
@@ -74,24 +91,28 @@ Detector::Detector(const string& model_file,
   num_channels_ = input_layer->channels();
   CHECK(num_channels_ == 3 || num_channels_ == 1)
     << "Input layer should have 1 or 3 channels.";
-  input_geometry_ = cv::Size(input_layer->width(), input_layer->height());
+  input_geometry_ = Size(input_layer->width(), input_layer->height());
 
   /* Load the binaryproto mean file. */
   SetMean(mean_file, mean_value);
 }
 
-std::vector<vector<float> > Detector::Detect(const cv::Mat& img) {
+std::vector<vector<float> > Detector::Detect(const Mat& img) {
   Blob<float>* input_layer = net_->input_blobs()[0];
   input_layer->Reshape(1, num_channels_,
                        input_geometry_.height, input_geometry_.width);
   /* Forward dimension change to all layers. */
   net_->Reshape();
 
-  std::vector<cv::Mat> input_channels;
+#if CPU_ONLY || !USE_OPENCV_GPU
+  std::vector<Mat> input_channels;
   WrapInputLayer(&input_channels);
-
   Preprocess(img, &input_channels);
-
+#else
+  WrapInputLayer(&input_channels_gpu);
+  Preprocess(img, &input_channels_gpu);
+#endif
+  
   net_->Forward();
 
   /* Copy the output layer to a std::vector */
@@ -114,7 +135,7 @@ std::vector<vector<float> > Detector::Detect(const cv::Mat& img) {
 
 /* Load the mean file in binaryproto format. */
 void Detector::SetMean(const string& mean_file, const string& mean_value) {
-  cv::Scalar channel_mean;
+  Scalar channel_mean;
   if (!mean_file.empty()) {
     CHECK(mean_value.empty()) <<
       "Cannot specify mean_file and mean_value at the same time";
@@ -128,23 +149,23 @@ void Detector::SetMean(const string& mean_file, const string& mean_value) {
       << "Number of channels of mean file doesn't match input layer.";
 
     /* The format of the mean file is planar 32-bit float BGR or grayscale. */
-    std::vector<cv::Mat> channels;
+    std::vector<Mat> channels;
     float* data = mean_blob.mutable_cpu_data();
     for (int i = 0; i < num_channels_; ++i) {
       /* Extract an individual channel. */
-      cv::Mat channel(mean_blob.height(), mean_blob.width(), CV_32FC1, data);
+      Mat channel(mean_blob.height(), mean_blob.width(), CV_32FC1, data);
       channels.push_back(channel);
       data += mean_blob.height() * mean_blob.width();
     }
 
     /* Merge the separate channels into a single image. */
-    cv::Mat mean;
-    cv::merge(channels, mean);
+    Mat mean;
+    merge(channels, mean);
 
     /* Compute the global mean pixel value and create a mean image
      * filled with this value. */
     channel_mean = cv::mean(mean);
-    mean_ = cv::Mat(input_geometry_, mean.type(), channel_mean);
+    mean_ = Mat(input_geometry_, mean.type(), channel_mean);
   }
   if (!mean_value.empty()) {
     CHECK(mean_file.empty()) <<
@@ -159,15 +180,16 @@ void Detector::SetMean(const string& mean_file, const string& mean_value) {
     CHECK(values.size() == 1 || values.size() == num_channels_) <<
       "Specify either 1 mean_value or as many as channels: " << num_channels_;
 
-    std::vector<cv::Mat> channels;
+    std::vector<Mat> channels;
     for (int i = 0; i < num_channels_; ++i) {
       /* Extract an individual channel. */
-      cv::Mat channel(input_geometry_.height, input_geometry_.width, CV_32FC1,
-          cv::Scalar(values[i]));
+      Mat channel(input_geometry_.height, input_geometry_.width, CV_32FC1,
+          Scalar(values[i]));
       channels.push_back(channel);
     }
-    cv::merge(channels, mean_);
+    merge(channels, mean_);
   }
+  mean_gpu.upload(mean_);
 }
 
 /* Wrap the input layer of the network in separate cv::Mat objects
@@ -175,56 +197,109 @@ void Detector::SetMean(const string& mean_file, const string& mean_value) {
  * don't need to rely on cudaMemcpy2D. The last preprocessing
  * operation will write the separate channels directly to the input
  * layer. */
-void Detector::WrapInputLayer(std::vector<cv::Mat>* input_channels) {
+void Detector::WrapInputLayer(std::vector<Mat>* input_channels) {
   Blob<float>* input_layer = net_->input_blobs()[0];
 
   int width = input_layer->width();
   int height = input_layer->height();
   float* input_data = input_layer->mutable_cpu_data();
   for (int i = 0; i < input_layer->channels(); ++i) {
-    cv::Mat channel(height, width, CV_32FC1, input_data);
+    Mat channel(height, width, CV_32FC1, input_data);
     input_channels->push_back(channel);
     input_data += width * height;
   }
 }
 
-void Detector::Preprocess(const cv::Mat& img,
-                            std::vector<cv::Mat>* input_channels) {
+void Detector::WrapInputLayer(std::vector<gpu::GpuMat>* input_channels) {
+  Blob<float>* input_layer = net_->input_blobs()[0];
+
+  int width = input_layer->width();
+  int height = input_layer->height();
+  float* input_data = input_layer->mutable_gpu_data();
+  for (int i = 0; i < input_layer->channels(); ++i) {
+    gpu::GpuMat channel(height, width, CV_32FC1, input_data);
+    input_channels->push_back(channel);
+    input_data += width * height;
+  }
+}
+
+void Detector::Preprocess(const Mat& img,
+                            std::vector<Mat>* input_channels) {
   /* Convert the input image to the input image format of the network. */
-  cv::Mat sample;
+  Mat sample;
   if (img.channels() == 3 && num_channels_ == 1)
-    cv::cvtColor(img, sample, cv::COLOR_BGR2GRAY);
+    cvtColor(img, sample, COLOR_BGR2GRAY);
   else if (img.channels() == 4 && num_channels_ == 1)
-    cv::cvtColor(img, sample, cv::COLOR_BGRA2GRAY);
+    cvtColor(img, sample, COLOR_BGRA2GRAY);
   else if (img.channels() == 4 && num_channels_ == 3)
-    cv::cvtColor(img, sample, cv::COLOR_BGRA2BGR);
+    cvtColor(img, sample, COLOR_BGRA2BGR);
   else if (img.channels() == 1 && num_channels_ == 3)
-    cv::cvtColor(img, sample, cv::COLOR_GRAY2BGR);
+    cvtColor(img, sample, COLOR_GRAY2BGR);
   else
     sample = img;
 
-  cv::Mat sample_resized;
+  Mat sample_resized;
   if (sample.size() != input_geometry_)
-    cv::resize(sample, sample_resized, input_geometry_);
+    resize(sample, sample_resized, input_geometry_);
   else
     sample_resized = sample;
 
-  cv::Mat sample_float;
+  Mat sample_float;
   if (num_channels_ == 3)
     sample_resized.convertTo(sample_float, CV_32FC3);
   else
     sample_resized.convertTo(sample_float, CV_32FC1);
 
-  cv::Mat sample_normalized;
-  cv::subtract(sample_float, mean_, sample_normalized);
+  Mat sample_normalized;
+  subtract(sample_float, mean_, sample_normalized);
 
   /* This operation will write the separate BGR planes directly to the
-   * input layer of the network because it is wrapped by the cv::Mat
+   * input layer of the network because it is wrapped by the Mat
    * objects in input_channels. */
-  cv::split(sample_normalized, *input_channels);
+  split(sample_normalized, *input_channels);
 
   CHECK(reinterpret_cast<float*>(input_channels->at(0).data)
         == net_->input_blobs()[0]->cpu_data())
+    << "Input channels are not wrapping the input layer of the network.";
+}
+
+void Detector::Preprocess(const Mat& img,
+                            std::vector<gpu::GpuMat>* input_channels) {
+  /* Convert the input image to the input image format of the network. */
+  if(img.channels() == num_channels_)
+	  sample.upload(img);
+  else
+  { 
+	  g_img.upload(img);
+  	  if (g_img.channels() == 3 && num_channels_ == 1)
+		cvtColor(g_img, sample, COLOR_BGR2GRAY);
+	  else if (g_img.channels() == 4 && num_channels_ == 1)
+		cvtColor(g_img, sample, COLOR_BGRA2GRAY);
+	  else if (g_img.channels() == 4 && num_channels_ == 3)
+		cvtColor(g_img, sample, COLOR_BGRA2BGR);
+	  else if (g_img.channels() == 1 && num_channels_ == 3)
+		cvtColor(g_img, sample, COLOR_GRAY2BGR);
+  }
+
+  if (sample.size() != input_geometry_)
+    resize(sample, sample_resized, input_geometry_);
+  else
+    sample_resized = sample;
+
+  if (num_channels_ == 3)
+    sample_resized.convertTo(sample_float, CV_32FC3);
+  else
+    sample_resized.convertTo(sample_float, CV_32FC1);
+
+  subtract(sample_float, mean_gpu, sample_normalized);
+  
+  /* This operation will write the separate BGR planes directly to the
+   * input layer of the network because it is wrapped by the Mat
+   * objects in input_channels. */
+  split(sample_normalized, *input_channels);
+
+  CHECK(reinterpret_cast<float*>(input_channels->at(0).data)
+        == net_->input_blobs()[0]->gpu_data())
     << "Input channels are not wrapping the input layer of the network.";
 }
 
@@ -285,9 +360,15 @@ int main(int argc, char** argv) {
   // Process image one by one.
   std::ifstream infile(argv[3]);
   std::string file;
+  //Time Check
+  struct timeval start_point, end_point;
+  double operating_time;
+  gettimeofday(&start_point, NULL);  
+  int iFrame = 0;
+
   while (infile >> file) {
     if (file_type == "image") {
-      cv::Mat img = cv::imread(file, -1);
+      Mat img = imread(file, -1);
       CHECK(!img.empty()) << "Unable to decode image " << file;
       std::vector<vector<float> > detections = detector.Detect(img);
 
@@ -307,12 +388,13 @@ int main(int argc, char** argv) {
           out << static_cast<int>(d[6] * img.rows) << std::endl;
         }
       }
+      iFrame++;
     } else if (file_type == "video") {
-      cv::VideoCapture cap(file);
+      VideoCapture cap(file);
       if (!cap.isOpened()) {
         LOG(FATAL) << "Failed to open video: " << file;
       }
-      cv::Mat img;
+      Mat img;
       int frame_count = 0;
       while (true) {
         bool success = cap.read(img);
@@ -349,6 +431,9 @@ int main(int argc, char** argv) {
       LOG(FATAL) << "Unknown file_type: " << file_type;
     }
   }
+  gettimeofday(&end_point, NULL); 
+  operating_time = (double)(end_point.tv_sec)+(double)(end_point.tv_usec)/1000000.0-(double)(start_point.tv_sec)-(double)(start_point.tv_usec)/1000000.0;
+  printf("%d Frame, %f, %f fps\n", iFrame, operating_time, (float)iFrame / operating_time );
   return 0;
 }
 #else
